c8bfacb39d4e2b455947e33d6361362f45663a26
ayas <ahmdyaasiin@student.ub.ac.id>
2024-11-04 13:04:03 +0700

    feat: add Post controller for handling post-related actions

---
+++ controllers/Post.class.php
@@ -0,0 +1,15 @@
+<?php class Post extends Controller
+{
+
+    public function index()
+    {
+        // Load model
+        $postModel = $this->loadModel('PostModel');
+
+        // Get data from the model
+        $posts = $postModel->getAll();
+
+        // Load the view
+        $this->loadView('posts', ['posts' => $posts]);
+    }
+}
